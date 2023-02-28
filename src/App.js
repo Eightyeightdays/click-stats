ede9845df613caa8fdc97590f82b5d6d100b4767
Eightyeightdays <eightyeightdays@gmail.com>
2023-02-28 13:30:09 +0100

    Project initialised, basic data fetching in place

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [clicks1, setClicks1] = useState();
+
+  useEffect(()=>{
+    // fetch(`${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_ENDPOINT_1}`) // live link
+    fetch(`http://localhost:3000/${process.env.REACT_APP_ENDPOINT_1}`)               // local API
+    .then(data => data.json())
+    .then(json => setClicks1(json))
+    }, [] 
+
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ul>
+        {clicks1 && clicks1.map((element, key)=>{
+          return <li key={key}>{element.dateCreated}</li>
+        })}
+      
+      </ul>
+  
   );
 }
 
